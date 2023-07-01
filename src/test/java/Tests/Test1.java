95b73448d6cb1caac65bb1fd9b28f4c7ca24508a
Dmitriy Kazantsev <devidem@mail.ru>
2023-07-02 01:09:52 +0400

    TestNG+GHActs

---
+++ src/test/java/Tests/Test1.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test (groups = "second")
+    public void Amogus () {
+        System.out.println("Second");
+
+    }
+
+}
---
+++ src/test/java/Tests/Test2.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+    @Test (groups = "first")
+    public void Amogus () {
+        System.out.println("First");
+
+    }
+}

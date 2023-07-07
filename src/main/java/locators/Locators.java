4bfda8f7e51dd4543ca83600855ac66d46bac30c
Dmitriy Kazantsev <devidem@mail.ru>
2023-07-07 22:30:22 +0400

    Добавил многопоточную проверку

---
+++ src/main/java/locators/Locators.java
@@ -0,0 +1,26 @@
+package locators;
+
+public enum Locators {
+    SearchButton("//*[@class=\"css-1d9cswg e15krpzo1\"]/*[@type=\"submit\"]"),
+    Catalog ("//*[@class=\"css-3nmxdw eyoh4ac0\"]/*[@href=\"/catalog/\"]"),
+    SearchField ("//input[@type=\"search\"]"),
+    SearchWatchedBefore ("//*[contains(text(),'просмотренные')]"),
+    VarSearchResult("//*[contains(@href,\"" + "XPATH_VARIABLE" + "\")]//*[@data-meta-name=\"InstantSearchMainResult\"]"),
+    PopularCategory ("//*[@class=\"edhylph0 app-catalog-1ljlt6q e3tyxgd0\"]"),
+    ProductAbout ("//*[@data-meta-value = \"about\"]"),
+    VarProductPromoMain("//*[@data-meta-name=\"ProductHeaderContentLayout\"]//*[contains(text(),'" + "XPATH_VARIABLE" + "')]")
+    ;
+
+    public String getXpath() {
+        return xPath;
+    }
+    public String getXpathVariable (String xpathVariable) {
+        return getXpath().replace("XPATH_VARIABLE", xpathVariable);
+    }
+
+    private final String xPath;
+    Locators(String xPath) {
+        this.xPath = xPath;
+    }
+
+}
